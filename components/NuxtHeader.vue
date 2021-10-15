2fe31943cebe8e5cb60a59af600119a199ab7b93
SebastianDettmann <s.dettmann@meine-landausfluege.de>
2021-10-15 19:59:45 +0200

    Initial commit

---
+++ components/NuxtHeader.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "NuxtHeader"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/posts/_id/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="container posts-page">
+
+  </section>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+.posts-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
